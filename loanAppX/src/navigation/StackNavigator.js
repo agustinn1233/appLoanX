b7d24576cb65c20e05bdf1381cffbf3cd6af558c
agustinn1233 <71300843+agustinn1233@users.noreply.github.com>
2020-09-15 23:30:19 -0300

    [App] StackNavigation, tabBarNavigation and DrawerNavigation

---
+++ loanAppX/src/navigation/StackNavigator.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../screens/HomeScreen";
+
+const Stack = createStackNavigator();
+
+const MainStackNavigator = () => {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#9AC4F8",
+          },
+          headerTintColor: "white",
+          headerBackTitle: "Back",
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
+    );
+  }
+
+export { MainStackNavigator };
