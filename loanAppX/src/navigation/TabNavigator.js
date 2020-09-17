3f6345dde015cdffc98edbdf44e1e4a2b67ed8b1
Agustin Ferreira <agustinn1233@hotmail.com>
2020-09-17 15:32:55 -0300

    [App] New tabBar for navigation pages

--- loanAppX/src/navigation/TabNavigator.js
+++ loanAppX/src/navigation/TabNavigator.js
@@ -1,16 +1,41 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { MainStackNavigator, ContactStackNavigator } from "./StackNavigator";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={MainStackNavigator} />
-      <Tab.Screen name="Contact" component={ContactStackNavigator} />
-    </Tab.Navigator>
-  );
+    return (
+      <Tab.Navigator
+        initialRouteName="Feed"
+        tabBarOptions={{
+          activeTintColor: '#e91e63',
+        }}
+      >
+        <Tab.Screen
+          name="Feed"
+          component={MainStackNavigator}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={ContactStackNavigator}
+          options={{
+            tabBarLabel: 'Updates',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="bell" color={color} size={size} />
+            ),
+            tabBarBadge: 3,
+          }}
+        />
+      </Tab.Navigator>
+    );
 };
 
 export default BottomTabNavigator;
